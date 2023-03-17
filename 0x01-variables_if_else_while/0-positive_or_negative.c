82ecf01153aa68d53bd9f0a727bd28ea1d25604f
Diana Sungu <sungudiana1@gmail.com>
2023-03-17 12:26:23 -0700

    0x01. C - Variables, if, else, while

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <time.h>
+/** main - a program that prints output as positive or negative using the printf function
+ * Return: 0
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+
+	return (0);
+}
