269846b6a9c9dc907e430ce331145e7ba349f36d
Diana Sungu <sungudiana1@gmail.com>
2023-03-21 06:16:22 -0700

    main.h prototype files

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main - a program that prints output in lowercase
+ * Return: 0
+ */
+void print_alphabet(void);
+{
+char n;
+for(n = "a"; n <= "z"; n++)
+{
+_putchar(n);
+}
+_putchar('/n')
+}
