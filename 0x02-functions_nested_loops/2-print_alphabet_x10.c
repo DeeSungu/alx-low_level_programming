85d98a1ae9093ceaa0982bffcd17d07b7b34f110
Diana Sungu <sungudiana1@gmail.com>
2023-03-21 07:37:20 -0700

    a function that prints 10 times the alphabet, in lowercase

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,7 +2,7 @@
 /**
  * print_alphabet_x10 - afunction that prints alphabets in lowercase
  */
-void print_alphabet_x10(void);
+void print_alphabet_x10(void)
 {
 int g, h;
 for (g = 0; g <= 10; g++)
