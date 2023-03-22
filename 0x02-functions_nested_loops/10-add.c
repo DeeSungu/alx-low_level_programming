14266ec41adfae11cad115c77bf689be14134a56
Diana Sungu <sungudiana1@gmail.com>
2023-03-21 17:14:58 -0700

    a function that adds to integers and prints results

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,21 +1,13 @@
 #include "main.h"
 /**
  * add - adds two integers and print the result
- * Result: result
+ * Result: sum
  * @num1: the output to be extracted
  * @num2: the output to be extracted
  */
 int add(int num1, int num2)
 {
-int result = num1 + num2;
-if (result < 0)
-{
-_putchar('-');
-result = -result;
-}
-if (result > 9)
-{
-add(result / 10, 0);
-}
-_putchar('0' + result % 10);
+int sum;
+sum = num1 + num2;
+return (sum);
 }
