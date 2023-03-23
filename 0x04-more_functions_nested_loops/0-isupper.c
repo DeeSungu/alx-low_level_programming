e326c36eb0c1d8c7379bc734b9f9947efefabf0d
Diana Sungu <sungudiana1@gmail.com>
2023-03-23 13:54:19 -0700

    Debugging a program that prits if character is uppercase

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,17 +1,17 @@
 #include "main.h"
 /**
- * int _isupper - program that checks for uppercase character
- * @c - output to be extracted
- * Return: 0
+ * _isupper - program that checks for uppercase character
+ * @c: output to be extracted
+ * Return: 0 or 1
  */
 int _isupper(int c)
 {
-	if(c > "A" && c < "Z")
+	if (c > "A" && c < "Z")
 	{
-	return(1);
+	return (1);
 	}
 	else
 	{
-	return(0);
+	return (0);
 	}
 }
