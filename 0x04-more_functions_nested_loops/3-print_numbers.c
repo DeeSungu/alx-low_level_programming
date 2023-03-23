cde29d2f562f8104f53e0e5e284c687c7390e737
Diana Sungu <sungudiana1@gmail.com>
2023-03-23 15:14:15 -0700

    function that prints numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_numbers - function that prints numbers
+ * Return: always 0
+ */
+void print_numbers(void)
+{
+int i;
+if (i > 0, i < 9)
+{
+_putchar(i++);
+}
+_putchar("%d\n", i);
+}
