c41fcb63de4c34d6773dd6110241ca900e92b4fc
Diana Sungu <sungudiana1@gmail.com>
2023-03-27 06:40:46 -0700

    a function that prints a string, followed by a new line

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * _puts - a function that prints a string
+ * @str: string
+ * return: string
+ */
+void _puts(char *str)
+{
+str = "I love programming";
+_putchar ('\n');
+}
