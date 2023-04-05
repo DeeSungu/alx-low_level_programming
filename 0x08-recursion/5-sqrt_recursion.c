9aad4d43e1de530a43f5258f0d390745cc5eadf0
Diana Sungu <sungudiana1@gmail.com>
2023-04-05 11:21:03 -0700

    a function that returns 1 if the input integer is a prime number, otherwise return 0

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,32 +1,30 @@
 #include "main.h"
+int my_sqrt_recursion(int n, int i);
+
 /**
- * _sqrt_recursion - returns natural square root
- * @n: input number
- * Return: root
+ * _sqrt_recursion - returns the natural square root of a number
+ * @n: number to calculate the square root of
+ * Return: the result
  */
 int _sqrt_recursion(int n)
 {
-	int root;
-
 	if (n < 0)
-	{
-	return (-1);
-	}
-	else if (n == 0 || n == 1)
-	{
-	return (n);
-	}
-	else
-	{
-	root = _sqrt_recursion(n / 2);
-
-	if (n % 2 == 0)
-	{
-	return (root * 2);
-	}
-	else
-	{
-	return (root * 2 + 1);
-	}
-	}
+		return (-1);
+	return (my_sqrt_recursion(n, 0));
 }
+
+/**
+ * my_sqrt_recursion - recurses
+ * @n: number to calculate the sqaure root of
+ * @i: iterator
+ * Return: the result
+ */
+int my_sqrt_recursion(int n, int i)
+{
+	if (i * i > n)
+		return (-1);
+	if (i * i == n)
+		return (i);
+	return (my_sqrt_recursion(n, i + 1));
+}
+
