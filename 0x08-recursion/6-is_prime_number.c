90728a9258bd818d1adb24a84354b42125848de2
Diana Sungu <sungudiana1@gmail.com>
2023-04-05 10:57:34 -0700

    a function that returns 1 if the input integer is a prime number, otherwise return 0

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,33 +1,24 @@
-#include "main.h"
-/**
- * is_prime_number - returns values 1 or 0 if prime n.o
- * @n: input number
- * Return: 1 or 0;
- */
-int is_prime_number(int n)
-	{
-	if (n <= 1)
-	{
-	return (0);
-	}
-	else if (n <= 3)
-	{
-	return (1);
-	}
-	else if (n % 2 == 0 || n % 3 == 0)
-	{
-	return (0);
-	}
-	int max_divisor = sqrt(n);
-	int divisor = 5;
-
-	while (divisor <= max_divisor)
-	{
-	if (n % divisor == 0 || n % (divisor + 2) == 0)
-	{
-	return (0);
-	}
-	divisor += 6;
-	}
-	return (1);
+#include "main.h"                                                                                                                     
+                                                                                                                                      
+int is_divisible(int num, int div);                                                                                                   
+int is_prime_number(int n);                                                                                                           
+                                                                                                                                      
+/**                                                                                                                                   
+ * is_divisible - Checks if a number is divisible.                                                                                    
+ * @num: The number to be checked.                                                                                                    
+ * @div: The divisor.                                                                                                                 
+ *                                                                                                                                    
+ * Return: If the number is divisible - 0.                                                                                            
+ *         If the number is not divisible - 1.                                                                                        
+ */                                                                                                                                   
+int is_divisible(int num, int div)                                                                                                    
+{                                                                                                                                     
+	if (num % div == 0)                                                                                                           
+	return (0);                                                                                                           
+                                                                                                                                      
+	if (div == num / 2)                                                                                                           
+	return (1);                                                                                                           
+                                                                                                                                      
+	return (is_divisible(num, div + 1));                                                                                          
 }
+
