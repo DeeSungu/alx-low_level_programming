762e37cc1f22593a57a76009f14ced93e7cdce5c
Diana Sungu <sungudiana1@gmail.com>
2023-04-10 13:59:29 -0700

    a function that prints all arguments pased to it on a new line

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - prints all arguments passed to it
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: 0;
+ */
+int main(int argc, char *argv[])
+{
+	int a;
+
+	for (a = 0; a < argc; a++)
+	{
+		printf("%s\n", argv[a]);
+	}
+
+	return (0);
+}
+
