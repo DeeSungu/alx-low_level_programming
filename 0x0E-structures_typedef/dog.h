b237441eb047488e915e199002458c8f4ee17d9f
Diana Sungu <sungudiana1@gmail.com>
2023-04-17 05:51:33 -0700

    a program to define a type and add elements to it

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,24 +2,30 @@
 #define DOG_H
 
 /**
- * struct dog - the dogs information
- * @name: First
- * @age: Second
- * @owner: Third
+ * struct dog - a dog's basic info
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
  *
  * Description: Longer description
  */
 struct dog
 {
-	char* name
-	float* age
-	char* owner
+	char *name;
+	float age;
+	char *owner;
 };
 
-int main()
-{
-	struct dog my_dog = { "Poppy", 3.5, "Bob" };
-	printf("My dog's name is %s, they are %f years old, and their owner is %s.\n", my_dog.name, my_dog.age, my_dog.owner);
-	return 0;
-}
+/**
+ * my_dog - typedef for struct dog
+ */
+typedef struct dog my_dog;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+char *_strcpy(char *dest, char *src);
+int _strlen(char *s);
+
 #endif
