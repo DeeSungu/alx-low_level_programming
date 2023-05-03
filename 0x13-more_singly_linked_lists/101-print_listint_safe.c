28bf16633be15359d8ffab91e5f093b0e4daa5ce
Diana Sungu <sungudiana1@gmail.com>
2023-05-02 17:41:39 -0700

    More

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,38 +1,71 @@
 #include "lists.h"
 /**
- * print_listint_safe - the function
+ * myfunction - function
+ * @head: head
+ * Return: return
+ */
+size_t myfunction(const listint_t *head)
+{
+const listint_t *a, *h;
+size_t no = 1;
+
+if (head == NULL || head->next == NULL)
+return (0);
+
+a = head->next;
+h = (head->next)->next;
+
+while (h)
+{
+if (a == h)
+{
+a = head;
+while (a != h)
+{
+no++;
+a = a->next;
+h = h->next;
+}
+a = a->next;
+while (a != h)
+{
+no++;
+a = a->next;
+}
+return (no);
+}
+a = a->next;
+h = (h->next)->next;
+}
+return (0);
+}
+
+/**
+ * print_listint_safe - function
  * @head: head
  * Return: return
  */
 size_t print_listint_safe(const listint_t *head)
 {
-size_t c = 0;
-const listint_t *cu = head;
-const listint_t *start = NULL;
+size_t no = 0;
+const listint_t *cu = head, *s = head;
 
-	while (cu != NULL)
-	{
-	c++;
-	if (cu >= head + c)
-	{
-	start = cu;
-	break;
-	}
-	cu = cu->next;
-	}
+if (head == NULL)
+exit(98);
 
-	cu = head;
+while (cu)
+{
+no++;
+printf("[%p] %d\n", (void *)cu, cu->n);
+cu = cu->next;
 
-	while (cu != NULL)
-	{
-	printf("[%p] %d\n", (void *)cu, cu->n);
-	if (current == cycle_start)
-	{
-	printf("-> [%p] %d\n", (void *)start, start->n);
-	exit(98);
-	}
-	cu = cu->next;
-	}
+if (cu >= s)
+{
+printf("-> [%p] %d\n", (void *)cu, cu->n);
+no++;
+break;
+}
+}
 
-return (c);
+return (no);
 }
