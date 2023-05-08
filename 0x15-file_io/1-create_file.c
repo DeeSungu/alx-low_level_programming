481cde94291435d290d755d4fac00d2f48af075c
Diana Sungu <sungudiana1@gmail.com>
2023-05-08 15:38:51 -0700

    a function that creates a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -21,7 +21,7 @@ int create_file(const char *filename, char *text_content)
 	length++;
 	}
 
-	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0700);
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	w = write(fd, text_content, length);
 
 	if (fd == -1 || w == -1)
