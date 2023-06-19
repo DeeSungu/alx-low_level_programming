4920735224d9e34055d43bfe9c42be82f8209155
Diana Sungu <sungudiana1@gmail.com>
2023-06-19 20:45:32 +0300

    C - Dynamic libraries

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -O win.so https://github.com/kcoovi/alx-low_level_programming/raw/master/0x18-dynamic_libraries/win.so
+wget -O win.so https://github.com/DeeSungu/alx-low_level_programming/blob/master/0x18-dynamic_libraries/win.so
 export LD_PRELOAD=$(pwd)/win.so
