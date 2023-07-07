9e048610fa3866c6c19c0043192693ec5a3f941a
Diana Sungu <sungudiana1@gmail.com>
2023-07-07 14:00:35 +0300

    A function that creates a hash table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,8 +1,8 @@
 #include "hash_tables.h"
 /**
- * hash_table_create - program function
- * @size: hash table array size
- * Return: ht
+ * hash_table_create - function
+ * @size: size of the hash table array
+ * Return: hash table
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
